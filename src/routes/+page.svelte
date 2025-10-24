09944ce13367bd9b13be0257642ad0df310ede46
Vladyslav Denysenko <8b.vladyslav.denysenko@gmail.com>
2025-10-24 18:37:05 +0200

    Add some more rendering information to throw at this thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,17 @@
 <script>
 	import { Map as WorldMap, TileLayer, Marker, Popup } from 'sveaflet';
-	const latLngs = [
-		[53.4505555556, 14.5591666667],
-		[50.2687553207, 19.1818316409],
-		[50.1303, 19.4381],
-		[53.13444444, 17.92166667],
-		[50.8422222222, 16.2919444444],
-		[54.5222222222, 18.5425]
-	];
+	const cells = fetch('./hackplay_cells.json')
+		.then((response) => response.json())
+		.catch((error) => console.error('Error fetching JSON:', error));
 </script>
 
 <div style="width:100%;height:500px;">
 	<WorldMap options={{ center: [51.505, 14.5591666667], zoom: 6 }}>
 		<TileLayer url={'https://tile.openstreetmap.org/{z}/{x}/{y}.png'} />
-		{#each latLngs as latLng}
-			<Marker {latLng} />
-		{/each}
+		{#await cells then cellsRes}
+			{#each cellsRes as cell}
+				<Marker latLng={[cell.cell_lat, cell.cell_lon]} />
+			{/each}
+		{/await}
 	</WorldMap>
 </div>
